4666e59f053d78ce523aaa15cd05c54772dfaaba
Jaden Johnson <jadenjohnson9@gmail.com>
2021-12-04 11:06:25 -0800

    update links

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -18,6 +18,8 @@
     <p></p>
     <router-link to="/delete">Delete</router-link>
     <p></p>
+    <router-link to="/">Home</router-link>
+    <p></p>
     <a href ='https://github.com/JadenJohnson227/creative-project-4'>Github repository</a>
   </div>
 </div>
